707d7f7e17d1422729a367f4301f9ce06a14bfe0
Tobias Heintz <tobias.heintz@alcemy.tech>
2021-12-03 22:50:13 +0100

    Bit more refactoring

--- day3.py
+++ day3.py
@@ -4,12 +4,15 @@ with open("day3-input.txt") as f:
     values = f.read().splitlines()
     values = [int(v, 2) for v in values]
 
+def num_bits(values: List, index: int) -> int:
+    return sum([1 if v & 1 << index else 0 for v in values])
+
 def most_common_bit(values: List, index: int) -> int:
-    return int(sum([1 if v & 1 << index else 0 for v in values]) > len(values) / 2)
+    return int(num_bits(values, index) >= len(values) / 2)
 
 # part 1
-gamma_list = [most_common_bit(values, b) for b in range(12)]
-gamma = int("".join(str(x) for x in reversed(gamma_list)), 2)
+gamma_list = reversed([most_common_bit(values, b) for b in range(12)])
+gamma = int("".join(str(x) for x in gamma_list), 2)
 epsilon = (gamma ^ 0xffff) & 0x0fff
 
 print(gamma, epsilon, gamma * epsilon)
