5edaac2d864c6705397234516e013b26ca777f35
Tobias Heintz <tobias.heintz@alcemy.tech>
2021-12-08 16:34:20 +0100

    Day 7 part 2

--- day7.py
+++ day7.py
@@ -8,3 +8,19 @@ with open("day7-input.txt") as f:
 med = int(median(values))
 diff = sum([abs(v - med) for v in values])
 print(diff)
+
+# part 2
+# there is probably a very clean math based solution for this, but I can't think
+# of it, so I'll brute-force the solution
+
+best_target = 0
+best_target_sum = 999999999
+
+for target in range(200, 1200):
+    diff = sum([sum(range(1, abs(v - target) + 1)) for v in values])
+
+    if diff < best_target_sum:
+        best_target = target
+        best_target_sum = diff
+
+print(best_target, best_target_sum)
