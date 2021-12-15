0773957dd53d5354f2b5b17bf9f6a0712f60df08
Tobias Heintz <tobias.heintz@alcemy.tech>
2021-12-15 10:36:37 +0100

    Day 9 part 2

--- day9.py
+++ day9.py
@@ -1,3 +1,5 @@
+from math import prod
+
 with open("day9-input.txt") as f:
     values = f.read().splitlines()
     values = [[int(v) for v in line] for line in values]
@@ -5,6 +7,7 @@ with open("day9-input.txt") as f:
 # part 1
 dim_x = len(values[0])
 dim_y = len(values)
+lowpoints = []
 
 total = 0
 for y in range(dim_y):
@@ -21,7 +24,35 @@ for y in range(dim_y):
         if y < dim_y - 1 and values[y][x] >= values[y + 1][x]:
             continue
 
-        print(f"lowpoint: x={x} y={y} value={values[y][x]}")
+        #print(f"lowpoint: x={x} y={y} value={values[y][x]}")
+        lowpoints.append((x, y))
         total += 1 + values[y][x]
 
 print(total)
+
+# part 2
+adjacencies = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+basin_sizes = []
+
+for lp in lowpoints:
+    basin_size = 0
+    candidates = [lp]
+    visited = []
+
+    while len(candidates) > 0:
+        basin_size += 1
+        x, y = candidates.pop()
+        visited.append((x, y))
+        adj_coords = [(x + adj_x, y + adj_y) for adj_x, adj_y in adjacencies \
+            if x + adj_x >= 0 and x + adj_x < dim_x and y + adj_y >= 0 and y + adj_y < dim_y]
+
+        adj_valid = [(adj_x, adj_y) for adj_x, adj_y in adj_coords \
+            if (adj_x, adj_y) not in visited and (adj_x, adj_y) not in candidates and values[adj_y][adj_x] != 9]
+        candidates.extend(adj_valid)
+
+        #print(x, y, adj_coords, adj_valid)
+
+    # print(lp, basin_size)
+    basin_sizes.append(basin_size)
+
+print(prod(sorted(basin_sizes, reverse=True)[0:3]))
