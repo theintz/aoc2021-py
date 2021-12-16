6ef5cf6b65d30e9c3e84dd23d1d12af41a5d22cc
Tobias Heintz <tobias.heintz@alcemy.tech>
2021-12-17 00:41:07 +0100

    Formatting

--- day12.py
+++ day12.py
@@ -4,9 +4,22 @@ with open("day12-input.txt") as f:
     values = f.read().splitlines()
     values = [v.split("-") for v in values]
 
-# part 1
-
 nodes = {}
+
+for orig, dest in values:
+    if orig in nodes:
+        nodes[orig].append(dest)
+    else:
+        nodes[orig] = [dest]
+
+    if dest in nodes:
+        nodes[dest].append(orig)
+    else:
+        nodes[dest] = [orig]
+
+print(nodes)
+
+# part 1
 paths = 0
 
 # I am ashamed to admit that I don't understand entirely why this works, but it does.
@@ -25,20 +38,6 @@ def traverse(node: str, path: List, visited: List):
         return
 
     [traverse(cand, path + [cand], visited + [node]) for cand in nodes[node]]
-
-
-for orig, dest in values:
-    if orig in nodes:
-        nodes[orig].append(dest)
-    else:
-        nodes[orig] = [dest]
-
-    if dest in nodes:
-        nodes[dest].append(orig)
-    else:
-        nodes[dest] = [orig]
-
-print(nodes)
 
 traverse("start", ["start"], [])
 print(paths)
