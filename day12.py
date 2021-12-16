3797b8d313162a2f0128e8bc03e27568b147ab0e
Tobias Heintz <tobias.heintz@alcemy.tech>
2021-12-16 23:09:41 +0100

    Day 12 part 1

---
+++ day12.py
@@ -0,0 +1,44 @@
+from typing import List
+
+with open("day12-input.txt") as f:
+    values = f.read().splitlines()
+    values = [v.split("-") for v in values]
+
+# part 1
+
+nodes = {}
+paths = 0
+
+# I am ashamed to admit that I don't understand entirely why this works, but it does.
+# However, I could not figure out what needs to be returned, so I have to resort to
+# a nasty global variable
+def traverse(node: str, path: List, visited: List):
+    global paths
+
+    # print("ENTRY", node, path, visited)
+    if node == "end":
+        # print("DONE", path)
+        paths += 1
+        return
+
+    if node.islower() and node in visited:
+        return
+
+    [traverse(cand, path + [cand], visited + [node]) for cand in nodes[node]]
+
+
+for orig, dest in values:
+    if orig in nodes:
+        nodes[orig].append(dest)
+    else:
+        nodes[orig] = [dest]
+
+    if dest in nodes:
+        nodes[dest].append(orig)
+    else:
+        nodes[dest] = [orig]
+
+print(nodes)
+
+traverse("start", ["start"], [])
+print(paths)
